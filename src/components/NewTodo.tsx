3135927b0b36e4407537634641a4a50a0ce4c8cc
buentellopc <buentellopc@gmail.com>
2022-09-26 11:39:53 -0500

    Working with refs and useRef

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -6,8 +6,16 @@ interface NewTodoProps {
 }
 
 const NewTodo = (props: NewTodoProps) => {
+  // useRef needs to know what type object is dealing with, it also needs an initial value
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+  // submit handler takes an argument that is of certain event type, benefit from the inline handler inferring that provides typescript to quickly know the type
   const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    // either use ? or !, types will differ with the operators
+    const enteredText = todoTextInputRef.current!.value;
+
     if (enteredText.trim().length === 0) {
       return;
     }
